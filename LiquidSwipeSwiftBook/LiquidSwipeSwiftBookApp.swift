8b882bde5ad2b930b1132e118cbad4f72caf2e84
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2022-02-07 20:09:12 +0300

    Initial Commit

---
+++ LiquidSwipeSwiftBook/LiquidSwipeSwiftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeSwiftBookApp.swift
+//  LiquidSwipeSwiftBook
+//
+//  Created by Эдуард on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeSwiftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
